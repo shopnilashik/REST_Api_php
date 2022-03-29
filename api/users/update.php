ad6d70294839222138cbf99ba068d6100723b20e
shopnilashik <shopnil.ashik@gmail.com>
2022-03-29 10:19:17 +0600

    new changes

---
+++ api/users/update.php
@@ -0,0 +1,38 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Users.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Users($db);
+
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  // Set ID to update
+  $post->id = $data->id;
+
+  $post->name = $data->name;
+  $post->email = $data->email;
+  $post->password = $data->password;
+
+  // Update post
+  if($post->update()) {
+    echo json_encode(
+      array('message' => 'User Updated')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'User Not Updated')
+    );
+  }
+
